387ab89e8376a70052c4b7fc51b05fe180d7ca7d
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-04-28 06:45:50 +0900

    updated

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+// stateの型
+interface TaskState {
+  idCount: number;
+  tasks: { id: number; title: string; completed: boolean }[];
+  selectedTask: { id: number; title: string; completed: boolean };
+  isModalOpen: boolean;
+}
+
+// stateの初期値
+const initialState: TaskState = {
+  idCount: 1,
+  tasks: [{ id: 1, title: "Task A", completed: false }],
+  selectedTask: { id: 0, title: "", completed: false },
+  isModalOpen: false,
+};
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState,
+  reducers: {
+    createTask: (state, action) => {
+      state.idCount++;
+      const newTask = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newTask, ...state.tasks];
+    },
+  },
+});
+
+export const { createTask } = taskSlice.actions;
+
+// コンポーネント側からuseSlectorを用いてselectTaskを指定することで
+// stateの値をコンポーネントに渡すことが可能
+export const selectTask = (state: RootState): TaskState["tasks"] =>
+  state.task.tasks;
+
+export default taskSlice.reducer;
