b15eceda2a21068e38b88d2940ac84108725842a
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-04-30 23:38:20 +0900

    updated

--- src/components/emotion/EmotionComponent.tsx
+++ src/components/emotion/EmotionComponent.tsx
@@ -4,29 +4,30 @@ import styled from "@emotion/styled";
 const EmotionComponent: React.FC = () => {
   interface ComponentProps {
     className?: string;
-    label: string;
+    label: boolean;
   }
 
   const Component: FC<ComponentProps> = ({ label, className }) => (
-    <div className={className}>{label}</div>
+    <div className={className}>{label ? "true" : "false"}</div>
   );
 
   const StyledComponent0 = styled(Component)`
-    color: ${(props) => (label === "Important" ? "red" : "green")};
+    color: ${({ label }) => (label ? "red" : "green")};
   `;
 
   const StyledComponent1 = styled(Component)({
     color: "red",
   });
 
-  const App = () => (
+  const isExpired = true;
+  return (
     <div>
-      <StyledComponent0 label="Important" />
-      <StyledComponent1 label="Yea! No need to re-type this label prop." />
+      1
+      <StyledComponent0 label={isExpired} />
+      2
+      <StyledComponent1 label={false} />
     </div>
   );
-
-  return <div></div>;
 };
 
 export default EmotionComponent;
