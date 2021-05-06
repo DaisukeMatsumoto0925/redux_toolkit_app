4a9e700e530fce805823b6fb2a833ea1c85a3f6b
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-05-06 22:26:42 +0900

    add createForm

--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -2,15 +2,18 @@ import { TextField } from "@material-ui/core";
 import { useForm } from "react-hook-form";
 import React from "react";
 import styles from "./TaskForm.module.scss";
+import { createTask } from "../taskSlice";
+import { useDispatch } from "react-redux";
 
 type Inputs = {
   taskTitle: string;
 };
 
 const TaskForm: React.FC = () => {
+  const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
   const handleCreate = (data: Inputs) => {
-    console.log(data);
+    dispatch(createTask);
     reset();
   };
   return (
