11c763b0f55f150aaacc9abe8c835a7ba3a708d8
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-05-08 21:54:19 +0900

    upd

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -1,15 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../../app/store";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AppThunk, RootState } from "../../app/store";
 
-// stateの型
 interface TaskState {
+  // taskが何個あるのかを管理する
   idCount: number;
+  // storeに保存するtaskの一覧
   tasks: { id: number; title: string; completed: boolean }[];
+  // taskのtitleを編集する際にどのtaskが選択されているか
   selectedTask: { id: number; title: string; completed: boolean };
+  // Modalを開くか閉じるかのフラグ
   isModalOpen: boolean;
 }
 
-// stateの初期値
 const initialState: TaskState = {
   idCount: 1,
   tasks: [{ id: 1, title: "Task A", completed: false }],
@@ -21,6 +23,7 @@ export const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
+    // taskの作成
     createTask: (state, action) => {
       state.idCount++;
       const newTask = {
@@ -30,14 +33,41 @@ export const taskSlice = createSlice({
       };
       state.tasks = [newTask, ...state.tasks];
     },
+    // taskの編集
+    editTask: (state, action) => {
+      // state.tasksの中から指定したtaskを抜き出す
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        // 抜き出したtaskのtitleを書き換える
+        task.title = action.payload.title;
+      }
+    },
+    // どのtaskを選択しているか
+    selectTask: (state, action) => {
+      state.selectedTask = action.payload;
+    },
+    // Modalを開くか閉じるかのフラグ
+    handleModalOpen: (state, action) => {
+      state.isModalOpen = action.payload;
+    },
   },
 });
 
-export const { createTask } = taskSlice.actions;
+export const {
+  createTask,
+  editTask,
+  selectTask,
+  handleModalOpen,
+} = taskSlice.actions;
 
-// コンポーネント側からuseSlectorを用いてselectTaskを指定することで
-// stateの値をコンポーネントに渡すことが可能
-export const selectTask = (state: RootState): TaskState["tasks"] =>
+export const selectTasks = (state: RootState): TaskState["tasks"] =>
   state.task.tasks;
+
+export const selectIsModalOpen = (state: RootState): TaskState["isModalOpen"] =>
+  state.task.isModalOpen;
+
+export const selectSelectedTask = (
+  state: RootState
+): TaskState["selectedTask"] => state.task.selectedTask;
 
 export default taskSlice.reducer;
